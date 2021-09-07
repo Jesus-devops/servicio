286f0bc5962cae9972a8420838fa6f099fbaa94e
Jesus Salvador B <61017245+Jesus-devops@users.noreply.github.com>
2021-09-07 17:33:42 -0500

    2

--- anexo-6y8/MaltaAlumno2.php
+++ anexo-6y8/MaltaAlumno2.php
@@ -2,6 +2,72 @@
 	session_start();
 	include ("../conexion.php");
 
+
+	$primaria="";
+	$secundaria="";
+	$prepa="";
+	$estudiosSuperiores="";
+	$fechaDeNacimiento="";
+	$lugarDeNacimiento="";
+	$peso="";
+	$estatura="";
+	$estadoCivil="";
+	$nHijos="";
+	$domicilioActual="";
+	$telefono="";
+	$tipoVivienda="";
+	$viviendaEs="";
+	$nPersonas="";
+	$parentesco="";
+	$vivira="";
+	$ingresoMFamiliar="";
+	$tuIngreso="";
+	$avisarNombre="";
+	$avisarTelefono="";
+
+
+
+
+	$primaria=$_POST['primaria'];
+	$secundaria=$_POST['secundaria'];
+	$prepa=$_POST['prepa'];
+	$estudiosSuperiores=$_POST['estudiosSuperiores'];
+	$fechaDeNacimiento=$_POST['fechaDeNacimiento'];
+	$lugarDeNacimiento=$_POST['lugarDeNacimiento'];
+	$peso=$_POST['peso'];
+	$estatura=$_POST['estatura'];
+	$estadoCivil=$_POST['estadoCivil'];
+	$nHijos=$_POST['nHijos'];
+	$domicilioActual=$_POST['domicilioActual'];
+	$telefono=$_POST['telefono'];
+	$tipoVivienda=$_POST['tipoVivienda'];
+	$viviendaEs=$_POST['viviendaEs'];
+	$nPersonas=$_POST['nPersonas'];
+	$parentesco=$_POST['parentesco'];
+	$vivira=$_POST['vivira'];
+	$ingresoMFamiliar=$_POST['ingresoMFamiliar'];
+	$tuIngreso=$_POST['tuIngreso'];
+	$avisarNombre=$_POST['avisarNombre'];
+	$avisarTelefono=$_POST['avisarTelefono'];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	$Sql="update extra set 
 	primaria='".$_POST['primaria']."',
 	secundaria='".$_POST['secundaria']."',
